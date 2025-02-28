2186822a4a5bb6bd1d10c1d32caf12ff8bcbae5b
Finn <finn.beimborn@mnd.thm.de>
2025-03-01 00:24:04 +0100

    Schritt 2

--- src/java/com/projekt/main/Main.java
+++ src/java/com/projekt/main/Main.java
@@ -1,5 +1,10 @@
 package src.java.com.projekt.main;
+
 public class Main {
+  private Main() {
+    throw new UnsupportedOperationException("Muss so");
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello there");
   }
