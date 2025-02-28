b01dc0f1dc098b3bb1b25cb76e7bacc68e0b1976
Finn <finn.beimborn@mnd.thm.de>
2025-03-01 00:18:19 +0100

    Schritt 1

--- src/java/com/projekt/main/Main.java
+++ src/java/com/projekt/main/Main.java
@@ -1,3 +1,4 @@
+package src.java.com.projekt.main;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello there");
---
+++ src/java/com/projekt/main/package-info.java
@@ -0,0 +1,1 @@
+package src.java.com.projekt.main;
